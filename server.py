4f48b9d5a19d1e50df5afa20eaf1be40a4338c99
Michal Charemza <michal@charemza.name>
2018-10-24 19:05:06 +0100

    (refactor) To have main function

--- server.py
+++ server.py
@@ -7,22 +7,27 @@ import asyncio
 import aioftp
 
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+def main():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
 
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
-logger.addHandler(handler)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
 
-context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS)
-context.load_cert_chain(f'{os.environ["HOME"]}/ssl.crt', keyfile=f'{os.environ["HOME"]}/ssl.key')
+    context = ssl.SSLContext(protocol=ssl.PROTOCOL_TLS)
+    context.load_cert_chain(f'{os.environ["HOME"]}/ssl.crt',
+                            keyfile=f'{os.environ["HOME"]}/ssl.key')
 
-loop = asyncio.get_event_loop()
-server = aioftp.Server(ssl=context)
-loop.run_until_complete(server.start('0.0.0.0', 8021))
+    loop = asyncio.get_event_loop()
+    server = aioftp.Server(ssl=context)
+    loop.run_until_complete(server.start('0.0.0.0', 8021))
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    loop.run_until_complete(server.close())
-    loop.close()
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        loop.run_until_complete(server.close())
+        loop.close()
+
+
+main()
