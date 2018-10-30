7ca3c82ea7f90eb1805f97470aa27d25d2780892
Michal Charemza <michal@charemza.name>
2018-10-30 17:49:36 +0000

    (feat) Initial infrastructure

---
+++ infra/lb_app_public.tf
@@ -0,0 +1,84 @@
+resource "aws_lb" "app_public" {
+  name               = "ftps3-app-public"
+  internal           = false
+  load_balancer_type = "network"
+  subnets            = ["${aws_subnet.main.id}"]
+}
+
+resource "aws_lb_listener" "app_public_command" {
+  load_balancer_arn = "${aws_lb.app_public.arn}"
+  port              = "${var.ftp_command_port}"
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.app_public_command.arn}"
+  }
+}
+
+resource "aws_lb_target_group" "app_public_command" {
+  name_prefix = "pb${var.ftp_command_port}"
+  port        = "${var.ftp_command_port}"
+  protocol    = "TCP"
+  vpc_id      = "${data.aws_vpc.main.id}"
+  target_type = "ip"
+
+  health_check {
+    interval = 10
+    port = "${var.ftp_command_port}"
+    protocol = "TCP"
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lb_listener" "app_public_data" {
+  count             = "${var.ftp_data_ports_count}"
+  load_balancer_arn = "${aws_lb.app_public.arn}"
+  port              = "${lookup(data.null_data_source.ftp_data_ports.*.outputs[count.index], "port")}"
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.app_public_data.*.arn[count.index]}"
+  }
+}
+
+resource "aws_lb_target_group" "app_public_data" {
+  count       = "${var.ftp_data_ports_count}"
+  name_prefix = "fp${lookup(data.null_data_source.ftp_data_ports.*.outputs[count.index], "port")}"
+  port        = "${lookup(data.null_data_source.ftp_data_ports.*.outputs[count.index], "port")}"
+  protocol    = "TCP"
+  vpc_id      = "${data.aws_vpc.main.id}"
+  target_type = "ip"
+
+  health_check {
+    interval = 10
+    port = "${var.ftp_command_port}"
+    protocol = "TCP"
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lb_target_group_attachment" "app_public_command" {
+  count            = "${var.subnet_hosts_count}"
+  target_group_arn = "${aws_lb_target_group.app_public_command.arn}"
+  port             = "${var.ftp_command_port}"
+  target_id        = "${lookup(data.null_data_source.app_ips.*.outputs[count.index], "ip")}"
+}
+
+resource "aws_lb_target_group_attachment" "app_public_data" {
+  count            = "${var.subnet_hosts_count * var.ftp_data_ports_count}"
+  target_group_arn = "${aws_lb_target_group.app_public_data.*.arn[count.index % var.ftp_data_ports_count]}"
+  port             = "${lookup(data.null_data_source.ftp_data_ports.*.outputs[count.index % var.ftp_data_ports_count], "port")}"
+  target_id        = "${lookup(data.null_data_source.app_ips.*.outputs[count.index % var.subnet_hosts_count], "ip")}"
+}
