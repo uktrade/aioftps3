26fa1654a14e4eaa973caf9ba13da0e7ad8e52b6
Michal Charemza <michal@charemza.name>
2018-11-09 16:59:24 +0000

    (test) Support passing arguments to tests, so can easily run fewer tests

--- tests.sh
+++ tests.sh
@@ -4,4 +4,4 @@ echo "Waiting for Minio..."
 wget --waitretry=1 --retry-connrefused  --no-check-certificate -O- -header="Accept: text/html" https://127.0.0.1:9000/ &> /dev/null
 echo "Minio is running"
 
-python -m unittest -v
+python -m unittest -v -- "$@"
