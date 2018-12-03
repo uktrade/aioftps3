1f7d737fa5330200e1bcb63b2f1c55edd42c4ae0
Michal Charemza <michal@charemza.name>
2018-12-03 09:57:56 +0000

    (feat) S3 access logging

--- infra/s3_app.tf
+++ infra/s3_app.tf
@@ -9,6 +9,11 @@ resource "aws_s3_bucket" "app" {
     }
   }
 
+  logging {
+    target_bucket = "${aws_s3_bucket.app_access_log.id}"
+    target_prefix = "${var.app_bucket}/"
+  }
+
   lifecycle_rule {
     enabled = true
     abort_incomplete_multipart_upload_days = 2
@@ -33,6 +38,53 @@ data "aws_iam_policy_document" "app" {
     resources = [
       "arn:aws:s3:::${aws_s3_bucket.app.id}",
       "arn:aws:s3:::${aws_s3_bucket.app.id}/*",
+    ]
+    condition {
+      test = "Bool"
+      variable = "aws:SecureTransport"
+      values = [
+        "false"
+      ]
+    }
+  }
+}
+
+resource "aws_s3_bucket" "app_access_log" {
+  bucket = "${var.app_bucket}-access-log"
+  acl    = "log-delivery-write"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  lifecycle_rule {
+    enabled = true
+    abort_incomplete_multipart_upload_days = 2
+  }
+}
+
+resource "aws_s3_bucket_policy" "app_access_log" {
+  bucket = "${aws_s3_bucket.app_access_log.id}"
+  policy = "${data.aws_iam_policy_document.app_access_log.json}"
+}
+
+data "aws_iam_policy_document" "app_access_log" {
+  statement {
+    effect = "Deny"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    actions = [
+      "s3:*",
+    ]
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.app_access_log.id}",
+      "arn:aws:s3:::${aws_s3_bucket.app_access_log.id}/*",
     ]
     condition {
       test = "Bool"
