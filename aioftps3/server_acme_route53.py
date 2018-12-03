7bcdcec78ba710980400fbd33db77e2e8da5e6ca
Michal Charemza <michal@charemza.name>
2018-12-03 12:42:17 +0000

    (refactor) Move SSL context generation to separate module
    This is so it can later contain code to create a new SSL context as needed

---
+++ aioftps3/server_acme_route53.py
@@ -0,0 +1,14 @@
+from ssl import (
+    PROTOCOL_TLSv1_2,
+    SSLContext,
+)
+
+
+def ssl_context_manager(cert_path, private_key_path):
+    ssl_context = SSLContext(PROTOCOL_TLSv1_2)
+    ssl_context.load_cert_chain(cert_path, keyfile=private_key_path)
+
+    def get_context():
+        return ssl_context
+
+    return get_context
